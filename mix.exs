cf5d42c0e165515053f24cd56ce1d0ad82f7ca7b
tomasz <pragmainline@gmail.com>
2019-08-26 11:06:34 +0200

    remove beaker

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Derivico.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :exprotobuf, :beaker],
+      extra_applications: [:logger, :exprotobuf],
       mod: {Derivico.Application, []}
     ]
   end
